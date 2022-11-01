71601bfa44db1115568db0a56d42602dd3e7316b
Kra038 <99765340+Kra038@users.noreply.github.com>
2022-11-01 13:27:08 -0500

    Update FrmBattle.Designer.cs

--- Project/Fall2020_CSC403_Project/FrmBattle.Designer.cs
+++ Project/Fall2020_CSC403_Project/FrmBattle.Designer.cs
@@ -18,7 +18,7 @@
 
     #region Windows Form Designer generated code
 
-    /// <summary>
+    <summary>
     /// Required method for Designer support - do not modify
     /// the contents of this method with the code editor.
     /// </summary>
